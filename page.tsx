9fb781050f0a14c3d131200badf537ddeecfec7b
Kinza-125 <ka8553200@gmail.com>
2024-10-13 00:27:04 -0700

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,11 +1,15 @@
-
+import cssStyle from "./about.module.css";
+console.log(cssStyle,"CSS-Style");
 import React from 'react'
-import Links from "./components/link"
-export default function Homepage (){
-  return(
+
+const Aboutpage = () => {
+  return (
     <div>
-      <Links/>
-      <h1 className="green">welcome to the Home page</h1>
+      
+    <h1 className={cssStyle.green}><b>This is my about page</b></h1>
+    <h2 className={cssStyle.yellow}><b>ABOUT</b></h2>
     </div>
   )
 }
+
+export default Aboutpage
